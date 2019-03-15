2426602465dcf3f1b9d32b50172a0ee1eea6d825
Jaakko <jaakko@kronogra.fi>
2019-03-15 14:11:27 +0200

    Re-formatted the documents and fixed bug
    bug:
    now the redirecting works

--- app.js
+++ app.js
@@ -6,75 +6,76 @@ const keypress = require('keypress');
 
 const app = express();
 const port = 3000;
-const hostname = '172.19.144.204';
-//const hostname = 'localhost';
+//const hostname = '172.19.144.204';
+const hostname = 'localhost';
 
 var people = [];
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     var cookies = req.cookies.ree;
-    if(cookies != null){
+    if (cookies != null) {
         var iscookie = false;
-        for(var i = 0; i < people.length; i++){
-            if(cookies == people[i]){
-                res.sendFile(__dirname+"/cue.html");
+        for (var i = 0; i < people.length; i++) {
+            if (cookies == people[i]) {
+                res.sendFile(__dirname + "/cue.html");
                 iscookie = true;
                 break;
             }
         }
-        if(!iscookie){
-        res.sendFile(path.join(__dirname+'/index.html'));
+        if (!iscookie) {
+            res.sendFile(path.join(__dirname + '/index.html'));
         }
     }
-    else{
-        res.sendFile(path.join(__dirname+'/index.html'));
+    else {
+        res.sendFile(path.join(__dirname + '/index.html'));
     }
-});    
+});
 
 app.post('/', (req, res) => {
     var cookies = req.cookies.ree;
-    if(req.body.name.length < 10){
-            console.log("A new person needs your help!");
-            people.push(req.body.name);
-            res.cookie('ree', req.body.name);
+    if (req.body.name.length < 10) {
+        console.log("A new person needs your help!");
+        people.push(req.body.name);
+        res.cookie('ree', req.body.name);
     }
-    
-            res.redirect('./');
-});
 
-app.post('/cue', (req,res) => {
-var response = 0;
-    for(var i = 0; i < people.length; i++){
-        if(req.body.name == people[i]){
-            response = i+1;
-            break;
-        }
-    }
-    if(response > 0)
-    res.send(response.toString());
-    else
     res.redirect('./');
 });
 
-app.listen(port, hostname, () => {
-console.log('Express app is listening at '+ hostname + ':' + port +'!');
-
-keypress(process.stdin);
-
-process.stdin.on('keypress', function(ch, key){
-    if(people.length > 0){
-        console.log(people.shift() + " needs help!");
-    }else{
-        console.log("Nobody needs your help :c");
+app.post('/cue', (req, res) => {
+    var response = 0;
+    for (var i = 0; i < people.length; i++) {
+        if (req.body.name == people[i]) {
+            response = i + 1;
+            break;
+        }
     }
-
+    if (response > 0)
+        res.send(response.toString());
+    else
+        res.send("redirect");
 });
 
-process.stdin.setRawMode(true);
-process.stdin.resume();
+app.listen(port, hostname, () => {
+    console.log('Express app is listening at ' + hostname + ':' + port + '!');
+
+    keypress(process.stdin);
+
+    process.stdin.on('keypress', function (ch, key) {
+        if(key && key.name == 'e')
+            process.exit(0);
+        if (people.length > 0) {
+            console.log(people.shift() + " needs help!");
+        } else {
+            console.log("Nobody needs your help :c");
+        }
+    });
+
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
 
 });
