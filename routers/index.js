c1b854622eed7e9568e503a3c751570c06afd86a
ajh657 <33629266+ajh657@users.noreply.github.com>
2019-03-25 15:59:11 +0200

    Xss Prevention fix #4.3

--- routers/index.js
+++ routers/index.js
@@ -45,7 +45,7 @@ router.post('/', (req, res) => {
             console.log(discription);
 
             var connection = require('../database/connection.js');
-            var sql = 'INSERT INTO tickets (name, description) VALUES (' + connection.escape(req.body.name) + ',' + connection.escape() + ')';
+            var sql = 'INSERT INTO tickets (name, description) VALUES (' + connection.escape(req.body.name) + ',' + connection.escape(discription) + ')';
             connection.query(sql, (err, result) => {
                 console.log(JSON.stringify(result));
                 if (err) throw err;
