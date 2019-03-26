e701a368777c4df03ade22e334a5941518ef17e2
Arttu Höylä <aahoo01@gmail.com>
2019-03-26 11:20:27 +0200

    Removed timers

--- routers/index.js
+++ routers/index.js
@@ -5,34 +5,28 @@ const path = require('path');
 router.get('/', (req, res) => {
     var cookies = req.cookies.ticketAppCookie;
     async function Respond() {
-        console.time("TicketResponce")
         var getCueNumber = require('../database/cueNumber');
         if (cookies != null) {
             var iscookie = false;
             var cueNumber = await getCueNumber(cookies);
             if (cueNumber > 0) {
                 res.sendFile(path.resolve(__dirname + "./../public/cue.html"));
-                console.timeEnd("TicketResponce");
                 iscookie = true;
             }
 
             else if (!iscookie) {
                 res.sendFile(path.resolve(__dirname + './../public/index.html'));
-                console.timeEnd("TicketResponce");
                 res.clearCookie("ticketAppCookie");
             }
         }
         else {
             res.sendFile(path.resolve(__dirname + './../public/index.html'));
-            console.timeEnd("TicketResponce");
         }
     }
     Respond();
 });
 
 router.post('/', (req, res) => {
-
-    console.time("TicketInsert");
 
     var namebool = false;
 
@@ -45,15 +39,12 @@ router.post('/', (req, res) => {
             var name = req.body.name;
             var description = req.body.description;
 
-            console.time("Parser");
             description = description.replace(/</g,"&lt;");
             description = description.replace(/>/g,"&gt;");
-            console.timeEnd("Parser");
 
             var connection = require('../database/connection.js');
             var sql = 'INSERT INTO tickets (name, description) VALUES (' + connection.escape(name) + ',' + connection.escape(description) + ')';
             connection.query(sql, (err, result) => {
-                console.timeEnd("TicketInsert");
                 console.log(JSON.stringify(result));
                 if (err) throw err;
             });
@@ -65,17 +56,14 @@ router.post('/', (req, res) => {
 
 router.post('/cue', (req, res) => {
     async function GetCueNumber() {
-        console.time("GetCue");
         var getCue = require('../database/cueNumber');
         var response = await getCue(req.body.name);
         if (response > 0){
             res.send(response.toString());
-            console.timeEnd("GetCue");
         }
         else {
             res.clearCookie("ticketAppCookie");
             res.send("redirect");
-            console.timeEnd("GetCue");
         }
     }
     GetCueNumber();
